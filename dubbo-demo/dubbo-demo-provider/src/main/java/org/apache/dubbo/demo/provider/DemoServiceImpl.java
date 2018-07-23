6763982a2d328139e41366a65d711b67ce594a3c
xuezhy <lxy5822>
2018-07-23 21:35:58 +0800

    添加打印

--- dubbo-demo/dubbo-demo-provider/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/org/apache/dubbo/demo/provider/DemoServiceImpl.java
@@ -27,7 +27,10 @@ public class DemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String name) {
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+        System.out.println("xzy111");
         return "Hello " + name + ", response from provider: " + RpcContext.getContext().getLocalAddress();
-    }
 
+
+
+    }
 }
